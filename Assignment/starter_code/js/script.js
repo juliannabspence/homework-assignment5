b663a98df55d9c33b0e51fdd8a195c9d0e33a53b
juliannabspence <julianna.spence@gmail.com>
2017-08-08 15:45:11 -0700

    final homework

--- Assignment/starter_code/js/script.js
+++ Assignment/starter_code/js/script.js
@@ -1,48 +1,61 @@
 // Pseudocode:
 
 // User inputs value into input form and clicks submit
-// Define city variables
-// On click, change body background to the coordinating city "value" chosen
-// When NYC/New York/New York City is submitted, change background to .ny
-// When SF/San Fran/San Francisco is submitted, change background to .sf
-// When LA/Los Angeles is submitted, change background to .la
-// When Austin is submitted, change background to .austin
-// When Sydney/SYD is submitted, change background to .sydney
+// Change body background to the coordinating city "value" chosen
+// If NYC/New York/New York City is submitted, change background to .ny
+// If SF/San Fran/San Francisco is submitted, change background to .sf
+// If LA/Los Angeles is submitted, change background to .la
+// If Austin is submitted, change background to .austin
+// If Sydney/SYD is submitted, change background to .sydney
+// If "city" not recognized, alert "not recognized"
 
 
-
-
+// The Code:
 $(document).ready(function(){
 
-
-
 // User inputs value into input form and clicks submit
-$("input").val()
+$("#submit-btn").click(getInput);
 
-var city = "NYC"
+function getInput() {
+	event.preventDefault();
+	var input_city = $("input").val();
+	changeBackground(input_city);
+	$("#city-type").val("");
+	// console.log(city);
+}
+// Change body background to the coordinating city "value" chosen
+function changeBackground(city) {
+	console.log(city);
 
+// Use so if lowercase is entered computer will recognize city
+city = city.toLowerCase() // dont actaully fully understand this concept, read more
 
-console.log("input", city)
-// Listen for click 
-	//$("form").click(changeBackground)
-// Define city variable
-// On click, change body background to the "value" chosen
-// When NYC/New York/New York City is submitted, change background to .ny
-// When SF/San Fran/San Francisco is submitted, change background to .sf
-// When LA/Los Angeles is submitted, change background to .la
-// When Austin is submitted, change background to .austin
-// When Sydney/SYD is submitted, change background to .sydney
+// If NYC/New York/New York City is submitted, change background to .ny
+if (city == "new york" || city == "nyc" || city == "new york city") { 
+ 	$("body").attr("class", "nyc");
+ }
 
-	// function changeBackground(){ 
-		// event.preventDefault}
-	// $("body").click(changeBackground);
-	// var city == New York, NYC, New York City, SF, San Fran, San Francisco, LA, Los Angeles, Austin, SYD, Sydney
-	// if (city == New York) || (city == NYC) || (city == New York City) { $("body").attr("class", "nyc")}
-	// if (city == SF || (city == San Fran) || (city = San Francisco) { $("body").attr("class", "sf")}
-	// if (city == LA || (city == Los Angeles) { $("body").attr("class", "la")}
-	// if (city == Austin) { $("body").attr("class", "austin")}
-	// if (city == SYD || (city == Sydney) { $("body").attr("class", "sydney")}
+// if (city == SF || (city == San Fran) || (city = San Francisco) { $("body").attr("class", "sf")}
+else if (city == "sf" || city == "san fran" || city == "san francisco") { 
+	$("body").attr("class", "sf");
+}	
+// If LA/Los Angeles is submitted, change background to .la
+else if (city == "la" || city == "los angeles") { 
+	$("body").attr("class", "la");
+}
+// If Austin is submitted, change background to .austin
+else if (city == "austin") { 
+	$("body").attr("class", "austin");
+}
+// If Sydney/SYD is submitted, change background to .sydney
+else if (city == "syd" || city == "sydney") { 
+	$("body").attr("class", "sydney");
+}
+// If "city" not recognized, alert "Invalid city. Try again"
+else {
+	alert("Invalid city. Try again.");
+}
 
-
+}
 
 });
