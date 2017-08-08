d82795822a53b440ce4f309aa7c21cdbf65a7e4d
juliannabspence <julianna.spence@gmail.com>
2017-08-08 08:45:40 -0700

    help please

---
+++ Assignment/starter_code/js/script.js
@@ -0,0 +1,48 @@
+// Pseudocode:
+
+// User inputs value into input form and clicks submit
+// Define city variables
+// On click, change body background to the coordinating city "value" chosen
+// When NYC/New York/New York City is submitted, change background to .ny
+// When SF/San Fran/San Francisco is submitted, change background to .sf
+// When LA/Los Angeles is submitted, change background to .la
+// When Austin is submitted, change background to .austin
+// When Sydney/SYD is submitted, change background to .sydney
+
+
+
+
+$(document).ready(function(){
+
+
+
+// User inputs value into input form and clicks submit
+$("input").val()
+
+var city = "NYC"
+
+
+console.log("input", city)
+// Listen for click 
+	//$("form").click(changeBackground)
+// Define city variable
+// On click, change body background to the "value" chosen
+// When NYC/New York/New York City is submitted, change background to .ny
+// When SF/San Fran/San Francisco is submitted, change background to .sf
+// When LA/Los Angeles is submitted, change background to .la
+// When Austin is submitted, change background to .austin
+// When Sydney/SYD is submitted, change background to .sydney
+
+	// function changeBackground(){ 
+		// event.preventDefault}
+	// $("body").click(changeBackground);
+	// var city == New York, NYC, New York City, SF, San Fran, San Francisco, LA, Los Angeles, Austin, SYD, Sydney
+	// if (city == New York) || (city == NYC) || (city == New York City) { $("body").attr("class", "nyc")}
+	// if (city == SF || (city == San Fran) || (city = San Francisco) { $("body").attr("class", "sf")}
+	// if (city == LA || (city == Los Angeles) { $("body").attr("class", "la")}
+	// if (city == Austin) { $("body").attr("class", "austin")}
+	// if (city == SYD || (city == Sydney) { $("body").attr("class", "sydney")}
+
+
+
+});
